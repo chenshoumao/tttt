5c54b13ae459c1f26e4924b7db34e21bde29950c
chenshoumao <1223991507@qq.com>
2018-08-09 10:26:25 +0800

    kk

--- src/main/java/com/csm/Test.java
+++ src/main/java/com/csm/Test.java
@@ -1,6 +1,8 @@
 package com.csm;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -24,7 +26,8 @@ public class Test extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		PrintWriter out = response.getWriter();
+		out.print("Hello World!!!");
 	}
 
 	/**
